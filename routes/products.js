48636a6ce7fe8b72438d8a734908be8b42524166
Vinitathakur-stack <svineeta119@gmail.com>
2022-01-10 13:43:31 +0530

    changes

--- routes/products.js
+++ routes/products.js
@@ -21,7 +21,7 @@ const product_controller = require('../controllers/product');
 // routes
 router.get('/', product_controller.all_products);
 //router.post('/create',product_controller.isLoggedIn,product_controller.uploadImg, product_controller.product_create);
-router.post('/create',VerifyToken , upload_controller.upload.array('productPictures'), product_controller.product_create);
+router.post('/create' , upload_controller.upload.array('productPictures'), product_controller.product_create);
 router.get('/:id', product_controller.product_details);
 router.put('/update/:id', product_controller.product_update);
 router.delete('/delete/:id', product_controller.product_delete);
