08f081ca3a9b48822574e8badec10c59cc9ae003
Vinitathakur-stack <svineeta119@gmail.com>
2022-01-10 13:51:57 +0530

    image view

--- app.js
+++ app.js
@@ -35,6 +35,7 @@ app.use((req, res, next) => {
 // body parser configuration
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/uploads', express.static('./uploads'));
 // app.use(express.urlencoded());
 // app.use(express.json());
 
